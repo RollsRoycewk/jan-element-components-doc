e6e99e41a4f9d1b16dea392d3bbb8323ff26ce57
RollsRoycewk <65430895+RollsRoycewk@users.noreply.github.com>
2023-03-07 21:52:02 +0800

    feat: 集成组件库并添加显示和交互

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,14 @@ import './style/var.css';
 import { h } from 'vue';
 import AnimationTitle from '../components/AnimationTitle.vue';
 
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+// // 如果您正在使用CDN引入，请删除下面一行。
+// import * as Icons from '@element-plus/icons-vue';
+
+import JanUI from 'jan-element-components';
+import 'jan-element-components/style.css';
+
 export default {
   ...Theme,
   Layout() {
@@ -10,5 +18,8 @@ export default {
       'home-features-after': () => h(AnimationTitle),
     });
   },
-  enhanceApp({ app }) {},
+  enhanceApp({ app }) {
+    app.use(ElementPlus);
+    app.use(JanUI);
+  },
 };
--- docs/chooseIcon/index.md
+++ docs/chooseIcon/index.md
@@ -1,1 +1,30 @@
 ## 图标选择器
+
+用户点击按钮,弹出框显示所有图标,点击图标可以复制图标代码
+
+### 基本用法
+
+<br>
+ <div style="padding:1em;margin:1em;border:1px solid #eee">
+    <m-choose-icon title="选择图标" v-model:visible="visible"></m-choose-icon>
+ </div>
+
+### 示例代码
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+let visible = ref<boolean>(false);
+</script>
+
+```js
+ <div style="padding:1em;margin:1em;border:1px solid #eee">
+    <m-choose-icon title="选择图标" v-model:visible="visible"></m-choose-icon>
+ </div>
+
+  <script lang="ts" setup>
+  import { ref } from "vue";
+
+  let visible = ref<boolean>(false);
+  </script>
+```
